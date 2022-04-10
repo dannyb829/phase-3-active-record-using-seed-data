716fd84227784be046ee27cae7f1487480eff9a6
Daniel <dannyb829@gmail.com>
2022-04-10 00:52:06 -0400

    commit tree be71828219b09598da268a3bd2cfa88ca778a26a

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,14 @@
 # Add a console message so we can see output when the seed file runs
+
+
+50.times.with_index do |i|
+    Game.create({title: Faker::Game.title, 
+        genre: Faker::Game.genre, 
+        platform: Faker::Game.platform, 
+        price: rand(0..60)}
+    )
+    puts "seed file ran  #{i + 1} times+++++++++++"
+
+end
+
+puts "wheeeww that was alot!!!!"
